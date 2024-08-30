81d9ee073defce3e23a7c79de1be2b5d27ea2122
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2024-08-30 23:45:42 +0900

    chore: Refactor photo selection screen and update backend image upload functionality

--- lib/screens/3_recognition_results.dart
+++ lib/screens/3_recognition_results.dart
@@ -1,145 +1,5 @@
-// // import 'package:flutter/material.dart';
-
-// // class ImageData {
-// //   final String imageUrl;
-// //   final String item;
-// //   final int price;
-
-// //   ImageData({required this.imageUrl, required this.item, required this.price});
-// // }
-
-// // class AISelectionScreen extends StatelessWidget {
-// //   const AISelectionScreen({super.key});
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     // Retrieve arguments as a Map
-// //     final Map<String, String> selectedCategories =
-// //         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-
-// //     // Example list of images with additional information
-// //     final List<ImageData> images = [
-// //       ImageData(imageUrl: 'https://via.placeholder.com/150', item: 'Jacket', price: 100),
-// //       ImageData(imageUrl: 'https://via.placeholder.com/150', item: 'Shirt', price: 50),
-// //     ];
-
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         title: const Text('AI選択画像'),
-// //       ),
-// //       body: Stack(
-// //         children: [
-// //           GridView.builder(
-// //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-// //               crossAxisCount: 2,
-// //             ),
-// //             itemCount: images.length,
-// //             itemBuilder: (context, index) {
-// //               return GestureDetector(
-// //                 onTap: () {
-// //                   Navigator.pushNamed(context, '/listing',
-// //                       arguments: images[index].imageUrl);
-// //                 },
-// //                 child: Column(
-// //                   children: [
-// //                     Image.network(images[index].imageUrl),
-// //                     Text(images[index].item),
-// //                     Text('\$${images[index].price}'),
-// //                   ],
-// //                 ),
-// //               );
-// //             },
-// //           ),
-// //           Align(
-// //             alignment: Alignment.bottomRight,
-// //             child: ElevatedButton(
-// //               onPressed: () {
-// //                 Navigator.pushNamed(context, '/listing', arguments: images[0].imageUrl);
-// //               },
-// //               child: const Text('デバッグ: 出品画面へ'),
-// //             ),
-// //           ),
-// //         ],
-// //       ),
-// //     );
-// //   }
-// // }
-
-// import 'package:flutter/material.dart';
-
-// class ImageData {
-//   final String imageUrl;
-//   final String item;
-//   final int price;
-
-//   ImageData({required this.imageUrl, required this.item, required this.price});
-// }
-
-// class Response {
-//   final List<ItemData> allItems;
-//   final List<ItemData> selectedItems;
-//   final double sum;
-
-//   Response({required this.allItems, required this.selectedItems, required this.sum});
-// }
-
-// class ItemData {
-//   final String imageName;
-//   final String item;
-//   final double price;
-
-//   ItemData({required this.imageName, required this.item, required this.price});
-// }
-
-// class AISelectionScreen extends StatelessWidget {
-//   const AISelectionScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Retrieve arguments as a Map
-//     final Map<String, dynamic> arguments =
-//         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-
-//     final String moneyAmount = arguments['money'];
-//     final Response responseData = arguments['response'];
-
-//     // Use the data from responseData instead of the example list
-//     final List<ItemData> images = responseData.allItems;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('AI選択画像'),
-//       ),
-//       body: Stack(
-//         children: [
-//           GridView.builder(
-//             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//               crossAxisCount: 2,
-//             ),
-//             itemCount: images.length,
-//             itemBuilder: (context, index) {
-//               return GestureDetector(
-//                 onTap: () {
-//                   Navigator.pushNamed(context, '/listing',
-//                       arguments: images[index].imageName);
-//                 },
-//                 child: Column(
-//                   children: [
-//                     Image.network(images[index].imageName), // Assuming imageName is a URL here
-//                     Text(images[index].item),
-//                     Text('\$${images[index].price.toStringAsFixed(0)}'),
-//                   ],
-//                 ),
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import '../models/types.dart';
 
 // Screen that displays a list of items passed from the previous page
@@ -153,7 +13,10 @@ class AISelectionScreen extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     final String money = args['money'];
     final Response itemDataList = args['data'];
-    var images = itemDataList.allItems;
+    List<ItemData> images = [];
+    for (var idx in itemDataList.selectedIdx) {
+      images.add(itemDataList.allItems[idx]);
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -168,7 +31,7 @@ class AISelectionScreen extends StatelessWidget {
             margin: const EdgeInsets.symmetric(vertical: 8.0),
             child: ListTile(
               leading: Image.network(
-                item.imageName,
+                item.image!.path,
                 width: 50,
                 height: 50,
                 fit: BoxFit.cover,
