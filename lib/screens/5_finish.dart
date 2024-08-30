e62d184e4e5c4c1f27d37b41e5f24101a4bfe8f0
Yuri Nakano <0210yurin@gmail.com>
2024-08-31 04:45:06 +0900

    changed background

--- lib/screens/5_finish.dart
+++ lib/screens/5_finish.dart
@@ -1,3 +1,49 @@
+// import 'package:flutter/material.dart';
+// import 'package:share/share.dart'; 
+
+// class CompleteScreen extends StatelessWidget {
+//   const CompleteScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Listing completed'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Text('The listing is complete!'),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.popUntil(context, ModalRoute.withName('/aiSelection'));
+//               },
+//               child: const Text('Continue selling'),
+//             ),
+//             const SizedBox(height: 10), 
+//             ElevatedButton(
+//               onPressed: () {
+//                 // Share functionality
+//                 Share.share('Check out my listing!');
+//               },
+//               child: const Text('Share'),
+//             ),
+//             const SizedBox(height: 10),
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.popUntil(context, ModalRoute.withName('/'));
+//               },
+//               child: const Text('Return to the Seller Pages'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:share/share.dart'; 
 
@@ -8,36 +54,95 @@ class CompleteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Listing completed'),
+        title: const Text(
+          'The selling process is complete',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+        elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('The listing is complete!'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('/aiSelection'));
-              },
-              child: const Text('Continue selling'),
-            ),
-            const SizedBox(height: 10), 
-            ElevatedButton(
-              onPressed: () {
-                // Share functionality
-                Share.share('Check out my listing!');
-              },
-              child: const Text('Share'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-              },
-              child: const Text('Return to the Seller Pages'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple.shade100, Colors.purple.shade400],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Your display is completed!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              const SizedBox(height: 30),
+              CustomButton(
+                text: 'Continue Selling',
+                onPressed: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/aiSelection'));
+                },
+              ),
+              const SizedBox(height: 15),
+              CustomButton(
+                text: 'Share',
+                onPressed: () {
+                  Share.share('Check out my listing!');
+                },
+              ),
+              const SizedBox(height: 15),
+              CustomButton(
+                text: 'Return to the Seller Pages',
+                onPressed: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        backgroundColor: Colors.deepPurple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        elevation: 5,
+        shadowColor: Colors.purple.shade300,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
